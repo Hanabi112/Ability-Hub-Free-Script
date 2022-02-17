32d4d71ab753de5eb92f510f83cc0be6b54e8b3c
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 22:18:52 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -3,7 +3,7 @@ local PlaceId = game.PlaceId
 if PlaceId == 8357510970 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Hub/main/AnimePunchingSimulator.lua"))()
 
-else if 7842028704 then
+elseif 7842028704 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 else
   	game.Players.LocalPlayer:kick("Not Have Game")
