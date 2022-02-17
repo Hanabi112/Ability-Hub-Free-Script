c1e8515d8652980f6a5b02f7ece7ed6afe99ba41
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 23:44:24 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -5,7 +5,7 @@ if PlaceId == 8357510970 then
 elseif PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 else
-  	game.Players.LocalPlayer:kick("มันไม่มีเกมนี้ไอควายโง่สถุน")
+  	game.Players.LocalPlayer:kick("มันไม่มีเกมนี้ ไอควายโง่สถุน")
     wait(1)
     game:Shutdown()
 end
