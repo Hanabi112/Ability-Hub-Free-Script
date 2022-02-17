efb2b5c56a24782e05fd2984b64a729324bd8017
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 19:43:41 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -2,7 +2,7 @@ local PlaceId = game.PlaceId
 
 if PlaceId == 8357510970 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Hub/main/AnimePunchingSimulator.lua"))()
-
+else
   	game.Players.LocalPlayer:kick("Not Have Game")
     wait(1)
     game:Shutdown()
