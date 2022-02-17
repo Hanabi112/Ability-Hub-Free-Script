d499be55149da00d66c8e3af3a8741141411900a
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-17 19:28:13 +0700

    Update and rename README.md to Abiliity-Hub.lua

---
+++ Abiliity-Hub.lua
@@ -0,0 +1,9 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 8357510970 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Hub/main/AnimePunchingSimulator.lua"))()
+
+  	game.Players.LocalPlayer:kick("Not Have Game")
+    wait(1)
+    game:Shutdown()
+end
