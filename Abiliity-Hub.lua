8f4c071165db5dbade40be36df338f74a50b7524
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-02-21 16:46:20 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -6,6 +6,8 @@ elseif PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 elseif PlaceId == 3823781113 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
+elseif PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/BloxFruit-AbilityHub/main/BloxFruits.lua"))()
 else
   	game.Players.LocalPlayer:kick("มันไม่มีเกมนี้ ไอควายโง่สถุน")
     wait(1)
