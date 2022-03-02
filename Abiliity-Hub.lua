08e93f010d1d3bab110963c6aed7d920c7733da7
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-03-02 14:48:32 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -4,8 +4,8 @@ local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   if _G.ScriptMode == "AutoKaiTan" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Kaitan/main/BloxFruits.lua"))()
-  elseif _G.ScriptMode == "Main" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Hub-BloxFruits/main/BloxFruits.lua"))()
+  else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/AbiltiyHub-BloxFruits/main/BloxFruits.lua"))()
 end
 elseif PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
