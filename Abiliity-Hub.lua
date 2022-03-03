3af84e913713c91b778fff96d2615f00187b2daa
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-03-03 17:02:39 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -1,20 +1,44 @@
 repeat task.wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 
-loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/Bruh/main/.lua")()
+
 
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   if _G.ScriptMode == "AutoKaiTan" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Kaitan/main/BloxFruits.lua"))()
   else
    loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/AbilityHub-BloxFruits/main/BloxFruits.lua")()
+          game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Working..."
+							}
+						)
 end
 elseif PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
+          game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Working..."
+							}
+						)
 elseif PlaceId == 3823781113 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
+          game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Working..."
+							}
+						)
 else
-  	game.Players.LocalPlayer:kick("มันไม่มีเกมนี้ ไอควายโง่สถุน")
+            game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Not Working..."
+							}
+						)
+  	game.Players.LocalPlayer:kick("No Have Game")
     wait(1)
     game:Shutdown()
 end
