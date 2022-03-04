e596248cf843e6bc61bbc4343a009d44c8c89a65
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-03-04 16:30:09 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -7,7 +7,7 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   if _G.ScriptMode == "AutoKaiTan" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Kaitan/main/BloxFruits.lua"))()
   else
-   loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/AbilityHub-BloxFruits/main/BloxFruits.lua")()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/AbilityHub_BloxFruits/main/BloxFruits.lua"))()
           game:GetService("StarterGui"):SetCore("SendNotification",
 							{
 								Title = "Ability Hub",
