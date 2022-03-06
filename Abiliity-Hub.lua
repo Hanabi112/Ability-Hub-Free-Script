495004196915628e01940589d80bb6b05cf4d28b
N0Fee1 <98322667+N0Fee1@users.noreply.github.com>
2022-03-06 13:39:50 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -5,37 +5,49 @@ local PlaceId = game.PlaceId
 
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   if _G.ScriptMode == "AutoKaiTan" then
+	          game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Working...",
+								Icon = "rbxassetid://8957234733"
+							}
+						)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Ability-Kaitan/main/BloxFruits.lua"))()
   else
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/AbilityHub_BloxFruits/main/BloxFruits.lua"))()
-          game:GetService("StarterGui"):SetCore("SendNotification",
+		          game:GetService("StarterGui"):SetCore("SendNotification",
 							{
 								Title = "Ability Hub",
-								Text = "Blox Fruit Ability Hub Working..."
+								Text = "Blox Fruit Ability Hub Working...",
+								Icon = "rbxassetid://8957234733"
 							}
 						)
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/AbilityHub_BloxFruits/main/BloxFruits.lua"))()
 end
 elseif PlaceId == 7842028704 then
+	          game:GetService("StarterGui"):SetCore("SendNotification",
+							{
+								Title = "Ability Hub",
+								Text = "Blox Fruit Ability Hub Working...",
+								Icon = "rbxassetid://8957234733"
+							}
+						)
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
-          game:GetService("StarterGui"):SetCore("SendNotification",
-							{
-								Title = "Ability Hub",
-								Text = "Blox Fruit Ability Hub Working..."
-							}
-						)
+
 elseif PlaceId == 3823781113 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
-          game:GetService("StarterGui"):SetCore("SendNotification",
+	          game:GetService("StarterGui"):SetCore("SendNotification",
 							{
 								Title = "Ability Hub",
-								Text = "Blox Fruit Ability Hub Working..."
+								Text = "Blox Fruit Ability Hub Working...",
+								Icon = "rbxassetid://8957234733"
 							}
 						)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
 else
             game:GetService("StarterGui"):SetCore("SendNotification",
 							{
 								Title = "Ability Hub",
-								Text = "Blox Fruit Ability Hub Not Working..."
+								Text = "Blox Fruit Ability Hub Not Working...",
+								Icon = "rbxassetid://8957234733"
 							}
 						)
   	game.Players.LocalPlayer:kick("No Have Game")
