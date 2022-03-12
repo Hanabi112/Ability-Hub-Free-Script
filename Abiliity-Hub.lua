c23fe7282269b94ba5822a223539de83951256de
Ability <98322667+N0Fee1@users.noreply.github.com>
2022-03-13 00:20:07 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -2,8 +2,6 @@ repeat task.wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 
 
-
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 		          game:GetService("StarterGui"):SetCore("SendNotification",
 							{
 								Title = "Ability Hub",
@@ -11,26 +9,14 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 								Icon = "rbxassetid://8957234733"
 							}
 						)
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
    loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/BloxFruits_Ability/main/BloxFruits.lua")()
 elseif PlaceId == 7842028704 then
-	          game:GetService("StarterGui"):SetCore("SendNotification",
-							{
-								Title = "Ability Hub",
-								Text = "Loading Script...",
-								Icon = "rbxassetid://8957234733"
-							}
-						)
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 
 elseif PlaceId == 3823781113 then
-	          game:GetService("StarterGui"):SetCore("SendNotification",
-							{
-								Title = "Ability Hub",
-								Text = "Loading Script...",
-								Icon = "rbxassetid://8957234733"
-							}
-						)
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
+
 else
             game:GetService("StarterGui"):SetCore("SendNotification",
 							{
@@ -41,3 +27,9 @@ else
 						)
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999/main/Roblox.Script.lua"))()
 end
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Ability Hub",
+    Text = "Loading Script Successfully",
+    Icon = "rbxassetid://8957234733"
+})
