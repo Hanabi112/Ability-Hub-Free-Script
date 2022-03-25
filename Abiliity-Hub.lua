3a0154671e6a335128becdf078ef1844c63dede7
Ability <98322667+N0Fee1@users.noreply.github.com>
2022-03-25 22:28:33 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -1,11 +1,10 @@
-local PlaceId = game.PlaceId
 repeat wait()
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
    loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/BloxFruits_Ability/main/BloxFruits.lua")()
-elseif PlaceId == 7842028704 then
+elseif game.PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 
-elseif PlaceId == 3823781113 then
+elseif game.PlaceId == 3823781113 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
 
 else
