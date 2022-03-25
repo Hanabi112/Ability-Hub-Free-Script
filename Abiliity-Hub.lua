4009d4bb628a28b2b6dcae047529ffa2cdf21e9c
Ability <98322667+N0Fee1@users.noreply.github.com>
2022-03-25 22:49:28 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -1,6 +1,16 @@
 repeat wait()
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+   if SelectGui == 1 then
    loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/BloxFruits_AbilityFix/main/BloxFruits.lua")()
+   elseif SelectGui == 2 then
+   loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/NewGui_BloxFruit/main/Abiliity-Hub.lua")()
+   else
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+          Title = "Ability Hub",
+          Text = "Don't Run Gui",
+          Icon = "rbxassetid://8957234733"
+      })
+end
 elseif game.PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
 elseif game.PlaceId == 3823781113 then
