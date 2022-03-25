e7fec2021606ddd225bc6abf5cbe5bef6875bbf8
Ability <98322667+N0Fee1@users.noreply.github.com>
2022-03-25 22:39:29 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -3,10 +3,8 @@ if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7
    loadstring(game:HttpGet"https://raw.githubusercontent.com/N0Fee1/BloxFruits_AbilityFix/main/BloxFruits.lua")()
 elseif game.PlaceId == 7842028704 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Anime-Clash-Simulator/main/Anime%20Clash%20Simulator.lua"))()
-
 elseif game.PlaceId == 3823781113 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/Saber-Simulator/main/Saber-Simulator.lua"))()
-
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/N0Fee1/9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999/main/Roblox.Script.lua"))()
 end
