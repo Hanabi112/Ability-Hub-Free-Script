f9ef7146c22bacbd2112290fbbc2b3a769233b86
Hanabi <98322667+Hanabi112@users.noreply.github.com>
2022-04-21 19:03:03 +0700

    Update Abiliity-Hub.lua

--- Abiliity-Hub.lua
+++ Abiliity-Hub.lua
@@ -229,6 +229,8 @@ end;
     
       if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
         loadstring(game:HttpGet"https://raw.githubusercontent.com/Hanabi112/NewGui_BloxFruit/main/Abiliity-Hub.lua")()
+      elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/Hanabi112/King_Lagacy/main/Abiliity-Hub.lua")()
       else
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/Hanabi112/9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999/main/Roblox.Script.lua")()
       end
